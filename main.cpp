e39db5516012d1e7c8b58a745f83713ad44e2bc9
sebbu <zsbe17fr@yahoo.fr>
2012-04-21 14:43:48 +0200

    show world on screen + collisions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,144 @@
+#include <vector>
 #include <cstring>
 #include <cstdio>
+// #include <cstdlib>
+// #include <cstddef>
+// #include <string>
+#include <cassert>
+#include <cerrno>
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL/SDL_gfxPrimitives.h>
 #include <SDL_ttf.h>
 #include <SDL_mixer.h>
 #include "main.hpp"
+
+using namespace std;
+
+const char *my_errors[64];
+
+const int status_height=32;
+const int width=384;
+const int height=384+status_height;
+
+typedef vector<vector<int> > vvi_t;
+typedef vector<vector<unsigned int> > vvui_t;
+
+int read_lvl(const char* name, vvui_t* level) {
+	//declare variables
+	int res=0;
+	char* fp_res=0;
+	FILE* fp=fopen(name, "rb");
+	char buffer[577];
+	char* buffer_p=buffer;
+	
+	//read height
+	fp_res=fgets(buffer, 576, fp);
+	if(fp_res==NULL) {
+		fprintf(stderr, "[ERROR] %d - %s\n", errno, my_errors[errno]);
+		return 1;
+	}
+	unsigned int l_height=(unsigned)atoi(buffer);
+	
+	//read width
+	fp_res=fgets(buffer, 576, fp);
+	if(fp_res==NULL) {
+		fprintf(stderr, "[ERROR] %d - %s\n", errno, my_errors[errno]);
+		return 1;
+	}
+	unsigned int l_width=(unsigned)atoi(buffer);
+	
+	//allocate level
+	level->resize(l_height);
+	for(unsigned int i=0;i<l_height;++i) {
+		level->at(i).resize(l_width);
+	}
+	
+	//read level data
+#if 0
+	fp_res=fgets(buffer, 576, fp);
+	if(fp_res==NULL) {
+		fprintf(stderr, "[ERROR] %d - %s\n", errno, my_errors[errno]);
+		return 1;
+	}
+	unsigned int buf_len=strlen(buffer);
+	assert( (buf_len >= 3*l_height*l_width) && (buf_len <= 4*l_height*l_width) );
+#endif
+	
+	//parse level data
+	unsigned short nb=0;
+	for(unsigned int i=0;i<l_height;++i) {
+#if 1
+		fp_res=fgets(buffer, 576, fp);
+		buffer_p=buffer;
+		if(fp_res==NULL) {
+			fprintf(stderr, "[ERROR] %d - %s\n", errno, my_errors[errno]);
+			return 1;
+		}
+		unsigned int buf_len=strlen(buffer);
+		assert( (buf_len >= 3*l_width) && (buf_len <= 4*l_width) );
+#endif
+		for(unsigned int j=0;j<l_width;++j) {
+			nb=0;
+			sscanf(buffer_p, "%hu", &nb);
+			assert(nb<16);
+			level->at(i).at(j)=(unsigned)nb;
+			buffer_p+=3;
+			if(nb>=10) ++buffer_p;
+		}
+	}
+	
+	//quit
+	fclose(fp);
+	return res;
+}
+
+/*
+#	L	R	LR
+U	UL	U	ULR
+D	DL	DR	DLR
+UD	UDL	UDR	UDLR
+*/
+
+inline unsigned int get_row(const SDL_Rect pos) {
+	return (unsigned)((pos.y-status_height)/32);
+}
+
+inline unsigned int get_col(const SDL_Rect pos) {
+	return (unsigned)(pos.x/32);
+}
+
+inline unsigned int get_collision(const vvui_t& level, unsigned int row, unsigned int col) {
+	return level.at(row).at(col);
+}
+
+inline bool can_go_left(const vvui_t& level, unsigned int row, unsigned int col) {
+	unsigned int src=get_collision(level, row, col);
+	if(col==0) return false;
+	unsigned int dst=get_collision(level, row, col-1);
+	return (((src%2)==0)&&((dst%4)<2));
+}
+
+inline bool can_go_right(const vvui_t& level, unsigned int row, unsigned int col) {
+	unsigned int src=get_collision(level, row, col);
+	if(col>level.at(0).size()) return false;
+	unsigned int dst=get_collision(level, row, col+1);
+	return (((src%4)<2)&&((dst%2)==0));
+}
+
+inline bool can_go_up(const vvui_t& level, unsigned int row, unsigned int col) {
+	unsigned int src=get_collision(level, row, col);
+	if(row==0) return false;
+	unsigned int dst=get_collision(level, row-1, col);
+	return (((src%2)==0)&&((dst%4)<2));
+}
+
+inline bool can_go_down(const vvui_t& level, unsigned int row, unsigned int col) {
+	unsigned int src=get_collision(level, row, col);
+	if(row>level.size()) return false;
+	unsigned int dst=get_collision(level, row+1, col);
+	return (((src%4)<2)&&((dst%2)==0));
+}
 
 int main(int argc, char* argv[]) {
 	printf("argc==%d\n", argc);
@@ -16,6 +149,13 @@ int main(int argc, char* argv[]) {
 		}
 		return 0;
 	}
+	my_errors[(int)EAGAIN]="EAGAIN";
+	my_errors[(int)EBADF]="EBADF";
+	my_errors[(int)EINTR]="EINTR";
+	my_errors[(int)EIO]="EIO";
+	//my_errors[(int)EOVERFLOW]="EOVERFLOW";
+	my_errors[(int)ENOMEM]="ENOMEM";
+	my_errors[(int)ENXIO]="ENXIO";
 	int error_code=0;
 	
 	//SDL init
@@ -34,7 +174,7 @@ int main(int argc, char* argv[]) {
 	
 	//SDL video
 	SDL_Surface *screen;
-	screen = SDL_SetVideoMode(800, 600, 32, SDL_SWSURFACE);
+	screen = SDL_SetVideoMode(width, height, 32, SDL_SWSURFACE);
 	++error_code;//3
 	if ( screen == NULL ) {
 		fprintf(stderr, "Unable to set 800x600 video: %s\n", SDL_GetError());
@@ -48,17 +188,29 @@ int main(int argc, char* argv[]) {
 	printf("white A : %u %u %u %u\n", white>>24, (white>>16)&0xFF, (white>>8)&0xFF, white&0xFF);
 	Uint32 st_blue=SDL_MapRGBA(screen->format,96,128,255,SDL_ALPHA_TRANSPARENT);
 	printf("st_blue A : %u %u %u %u\n", st_blue>>24, (st_blue>>16)&0xFF, (st_blue>>8)&0xFF, st_blue&0xFF);
+	Uint32 st_blue2=SDL_MapRGBA(screen->format,160,192,255,SDL_ALPHA_TRANSPARENT);
+	printf("st_blue2 A : %u %u %u %u\n", st_blue2>>24, (st_blue2>>16)&0xFF, (st_blue2>>8)&0xFF, st_blue2&0xFF);
 	Uint32 transparent=SDL_MapRGBA(screen->format, 255, 255, 255, SDL_ALPHA_TRANSPARENT);
 	printf("transparent A : %u %u %u %u\n", transparent>>24, (transparent>>16)&0xFF, (transparent>>8)&0xFF, transparent&0xFF);
 	SDL_SetColorKey(screen,SDL_SRCCOLORKEY,transparent);
 	
-	//image
-	SDL_Surface *fond=IMG_Load("152-Animal02.png");
+	//images
+	SDL_Surface *fond=IMG_Load("world_001.png");
+	SDL_Rect fond_pos={0,32,384,384};
 	++error_code;//4
 	if(fond==NULL) {
 		fprintf(stderr, "IMG_Load: %s\n", IMG_GetError());
 		exit(error_code);
 	}
+	SDL_Surface *player=IMG_Load("player2.png");
+	SDL_Rect player_pos={0,256,0,0};
+	player_pos.x=(Sint16)(player_pos.x+fond_pos.x);
+	player_pos.y=(Sint16)(player_pos.y+fond_pos.y);
+	if(player==NULL) {
+		fprintf(stderr, "IMG_Load: %s\n", IMG_GetError());
+		exit(error_code);
+	}
+	SDL_SetColorKey( player, SDL_SRCCOLORKEY, SDL_MapRGB(player->format, 255, 255, 255) );
 	
 	//font
 	TTF_Font *font;
@@ -69,8 +221,6 @@ int main(int argc, char* argv[]) {
 		/* handle error */
 		exit(error_code);
 	}
-	
-	
 	
 	// load support for the music formats
 	int flags=MIX_INIT_MP3|MIX_INIT_OGG|MIX_INIT_MOD|MIX_INIT_FLAC;
@@ -102,6 +252,21 @@ int main(int argc, char* argv[]) {
 	// allocate 16 mixing channels
 	Mix_AllocateChannels(16);
 	
+	//load level
+	vvui_t level;
+	int res=read_lvl("world_001.lvl", &level);
+	assert(res==0);
+	/*printf("cell(row,col)=value\n");
+	for(unsigned int i=0;i<level.size();++i) {//height
+		for(unsigned int j=0;j<level.at(0).size();++j) {//width
+			printf("cell(%02u,%02u)=%02d\n", i+1, j+1, level.at(i).at(j));
+		}
+	}//*/
+	
+	
+	//SDL_EnableKeyRepeat(10, 10);
+	SDL_EnableKeyRepeat(40, 20);
+	
 	//dialog
 	/*Dialogue dlg("");
 	ostringstream s;
@@ -110,17 +275,48 @@ int main(int argc, char* argv[]) {
 	
 	//game loop
 	
-	SDL_Event event;
-	
 	bool quitProgram=false;
-	bool playing=false;
+#if 1
+	bool playing=false;//music
+	SDL_Event event;
+#endif
+#if 0
+	Uint8 *state=NULL;
+	int key_count=0;
+	state = SDL_GetKeyState(&key_count);
+	printf("[INFO] key count = %d\n", key_count);
+	assert(key_count==SDLK_LAST);
+#endif
 	// duration of main loop
 	Uint32 duration = 40;
 	++error_code;//9
+	/*
+	0	L
+	1	R
+	2	U
+	3	D
+	*/
+	/*
+	-1	left
+	0	normal (unset)
+	+1	right
+	*/
+	char x_diff=0;
+	char x_diff_previous=0;
+	/*
+	-1	up
+	0	normal (unset)
+	+1	down
+	*/
+	char y_diff=0;
+	char y_diff_previous=0;
+	x_diff_previous+=0;
+	y_diff_previous+=0;
 	while (!quitProgram)
 	{
 		Uint32 ticks = SDL_GetTicks();
 		
+#if 1
 		//start events
 		while ( SDL_PollEvent(&event) ) {
 			switch (event.type) {
@@ -154,14 +350,108 @@ int main(int argc, char* argv[]) {
 						case SDLK_ESCAPE: // Escape pressed, exiting program
 							quitProgram=true;
 							break;
+						case SDLK_LEFT:
+							if(x_diff_previous>0) x_diff_previous=x_diff;
+							x_diff=-1;
+							break;
+						case SDLK_RIGHT:
+							if(x_diff_previous<0) x_diff_previous=x_diff;
+							x_diff= 1;
+							break;
+						case SDLK_UP:
+							if(y_diff_previous>0) y_diff_previous=y_diff;
+							y_diff=-1;
+							break;
+						case SDLK_DOWN:
+							if(y_diff_previous<0) y_diff_previous=y_diff;
+							y_diff= 1;
+							break;
 						default:
 							break;
 					}//end switch key
+					break;
+				case SDL_KEYUP:
+					switch(event.key.keysym.sym)
+					{
+						case SDLK_LEFT:
+							if(x_diff!=0) {
+								assert(x_diff_previous>=0);
+								x_diff=x_diff_previous;
+							}
+							x_diff_previous=0;
+							break;
+						case SDLK_RIGHT:
+							if(x_diff!=0) {
+								assert(x_diff_previous<=0);
+								x_diff=x_diff_previous;
+							}
+							x_diff_previous=0;
+							break;
+						case SDLK_UP:
+							if(y_diff!=0) {
+								assert(y_diff_previous>=0);
+								y_diff=y_diff_previous;
+							}
+							y_diff_previous=0;
+							break;
+						case SDLK_DOWN:
+							if(y_diff!=0) {
+								assert(y_diff_previous<=0);
+								y_diff=y_diff_previous;
+							}
+							y_diff_previous=0;
+							break;
+						default:
+							break;
+					}
 					break;
 				default:
 					break;
 			}
 		}//end events
+#endif
+
+#if 0
+		//events (bis)
+		SDL_PumpEvents();
+		if( state[SDLK_ESCAPE]!=0 ) {
+			quitProgram=true;
+		}
+		if( (state[SDLK_LEFT]==0) ^ (state[SDLK_RIGHT]==0) ) {
+			if( state[SDLK_LEFT]==1 ) {
+				x_diff=-1;
+			}
+			if( state[SDLK_RIGHT]==1 ) {
+				x_diff= 1;
+			}
+		}
+		else {
+			x_diff= 0;
+		}
+		if( (state[SDLK_UP]==0) ^ (state[SDLK_DOWN]==0) ) {
+			if( state[SDLK_UP]==1 ) {
+				y_diff=-1;
+			}
+			if( state[SDLK_DOWN]==1 ) {
+				y_diff= 1;
+			}
+		}
+		else {
+			y_diff= 0;
+		}
+#endif
+		
+		//parse event
+		if(x_diff!=0) {
+			if(x_diff<0 && !can_go_left(level, get_row(player_pos), get_col(player_pos))) break;
+			if(x_diff>0 && !can_go_right(level, get_row(player_pos), get_col(player_pos))) break;
+			player_pos.x=(Sint16)(player_pos.x+x_diff*8);
+		}
+		if(y_diff!=0) {
+			if(y_diff<0 && !can_go_up(level, get_row(player_pos), get_col(player_pos))) break;
+			if(y_diff>0 && !can_go_down(level, get_row(player_pos), get_col(player_pos))) break;
+			player_pos.y=(Sint16)(player_pos.y+y_diff*8);
+		}
 		
 		//clear screen
 		SDL_FillRect(screen, NULL, white); // white
@@ -169,26 +459,32 @@ int main(int argc, char* argv[]) {
 		//SDL_FillRect(screen, NULL, transparent); // transparent
 		//SDL_FillRect(screen, NULL, st_blue); // st_blue
 		
+		//draw labyrinth
+		SDL_BlitSurface(fond,NULL,screen,&fond_pos);
+		
 		//draw rounded box
-		roundedBoxColor(screen, 10, 10, 210, 210, 24, st_blue);
+		roundedBoxColor(screen, 4, 4, 108, 28, 4, st_blue);
+		roundedBoxColor(screen, 6, 6, 106, 26, 4, st_blue2);
 		
 		//draw text
 		SDL_Color _green={0,255,0,0};
-		SDL_Surface *text_surface = TTF_RenderText_Blended(font, "Hello World!", _green);
+		SDL_Surface *text_surface = TTF_RenderText_Blended(font, "HP: 100/100", _green);
 		if(text_surface!=NULL) {
 			SDL_Rect pos;
-			pos.x=25;
-			pos.y=25;
+			pos.x=12;
+			pos.y=12;
 			SDL_BlitSurface(text_surface,NULL,screen,&pos);
 			SDL_FreeSurface(text_surface);
 		}
 		
 		//draw image
 		{
-			SDL_Rect pos;
-			pos.x=225;
-			pos.y=225;
-			SDL_BlitSurface(fond,NULL,screen,&pos);
+			/*SDL_Rect pos_spr;
+			pos_spr.x=0;
+			pos_spr.y=64;
+			pos_spr.w=32;
+			pos_spr.h=32;*/
+			SDL_BlitSurface(player,NULL,screen,&player_pos);
 		}
 		
 		/* swap buffers */
